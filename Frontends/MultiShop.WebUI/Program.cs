ffe5fa9f8fbb5342d76a292f5b7c139ee2ec7054
hakanpakelgil <hakanpakelgil@hotmail.com>
2024-11-21 18:24:26 +0300

    progress

--- Frontends/MultiShop.WebUI/Program.cs
+++ Frontends/MultiShop.WebUI/Program.cs
@@ -1,6 +1,7 @@
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddHttpClient();
 builder.Services.AddControllersWithViews();
 
 var app = builder.Build();
@@ -22,6 +23,14 @@ app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=UILayout}/{action=_UILayout}/{id?}");
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+
+app.UseEndpoints(endpoints =>
+{
+    endpoints.MapControllerRoute(
+      name: "areas",
+      pattern: "{area:exists}/{controller=Home}/{action=Index}/{id?}"
+    );
+});
 
 app.Run();
