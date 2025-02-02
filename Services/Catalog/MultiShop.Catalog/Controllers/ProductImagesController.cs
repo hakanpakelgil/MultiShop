3ea617c34b99b33e4c9e8d03919fa512902eb7c8
hakanpakelgil <hakanpakelgil@hotmail.com>
2025-02-02 18:43:02 +0300

    progress

--- Services/Catalog/MultiShop.Catalog/Controllers/ProductImagesController.cs
+++ Services/Catalog/MultiShop.Catalog/Controllers/ProductImagesController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using MultiShop.Catalog.Dtos.ProductImageDtos;
 using MultiShop.Catalog.Services.ProductImageImageServices;
@@ -6,6 +7,7 @@ using MultiShop.Catalog.Services.ProductImageImageServices;
 
 namespace MultiShop.Catalog.Controllers
 {
+    [Authorize]
     [Route("api/[controller]")]
     [ApiController]
     public class ProductImagesController : ControllerBase
